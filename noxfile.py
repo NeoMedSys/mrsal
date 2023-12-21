183a3401838743bba88e0946b907946b29010fa9
Raafat <rafatzahran90@gmail.com>
2023-12-21 15:29:58 +0100

    fix  nox

--- noxfile.py
+++ noxfile.py
@@ -1,11 +1,40 @@
+import os
+import shutil
+
 import nox
 from nox.sessions import Session
 
 
 @nox.session()
+def setup(session: Session):
+    try:
+        root_path: str = os.getcwd()
+        # session.run("poetry", "install", "--with", "dev", external=True)
+        nox_path = os.path.join(root_path, ".nox")
+        reports_path = os.path.join(root_path, "reports")
+        coverage_path = os.path.join(reports_path, "coverage")
+        junit_path = os.path.join(reports_path, "junit")
+        ruff_path = os.path.join(reports_path, "ruff")
+        badges_path = os.path.join(reports_path, "badges")
+
+        if os.path.exists(nox_path):
+            shutil.rmtree(nox_path)
+        if os.path.exists(reports_path):
+            shutil.rmtree(reports_path)
+
+        os.makedirs(reports_path)
+        os.makedirs(junit_path)
+        os.makedirs(coverage_path)
+        os.makedirs(ruff_path)
+        os.makedirs(badges_path)
+    except Exception as e:
+        # Handle other exceptions
+        print(f"Error: {e}")
+
+
+@nox.session()
 def tests(session: Session):
     session.run("poetry", "install", "--with", "dev", external=True)
-    # session.run("poetry", "run", "pytest", "./tests", "--ignore", "./tests/test_ssl", "--junitxml=./reports/junit/junit.xml", external=True)
     # coverage
     session.run(
         "poetry",
@@ -42,6 +71,6 @@ def lint(session: Session):
 @nox.session()
 def gen_badge(session: Session):
     session.install("genbadge[tests,coverage,flake8]")
-    session.run("genbadge", "tests", "-i", "./reports/junit/junit.xml", "-o", "./doc_images/tests-badge.svg")
-    session.run("genbadge", "coverage", "-i", "./reports/coverage/coverage.xml", "-o", "./doc_images/coverage-badge.svg")
-    session.run("genbadge", "flake8", "-i", "./reports/ruff/ruff.txt", "-o", "./doc_images/ruff-badge.svg")
+    session.run("genbadge", "tests", "-i", "./reports/junit/junit.xml", "-o", "./reports/badges/tests-badge.svg")
+    session.run("genbadge", "coverage", "-i", "./reports/coverage/coverage.xml", "-o", "./reports/badges/coverage-badge.svg")
+    session.run("genbadge", "flake8", "-i", "./reports/ruff/ruff.txt", "-o", "./reports/badges/ruff-badge.svg")
