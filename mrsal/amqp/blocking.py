beacc03aa088b25a79c954fdc380ffc21f0ac0c2
JonNesvold <jon.nesvold@pm.me>
2024-09-11 19:31:14 +0200

    cleanliness yass

--- mrsal/amqp/blocking.py
+++ mrsal/amqp/blocking.py
@@ -1,12 +1,28 @@
 import pika
+from typing import Callable
+from pika.exceptions import AMQPConnectionError, ChannelClosedByBroker, StreamLostError, ConnectionClosedByBroker
 from pydantic.dataclasses import dataclass
 from mrsal.mrsal import Mrsal
+from neolibrary.monitoring.logger import NeoLogger
+from tenacity import retry, stop_after_attempt, wait_fixed, retry_if_exception_type, before_sleep_log
 
+log = NeoLogger(__name__, log_days=10)
 
 @dataclass
 class MrsalBlockingAMQP(Mrsal):
     use_blocking: bool = False
 
+    @retry(
+        retry=retry_if_exception_type(
+            AMQPConnectionError,
+            ChannelClosedByBroker,
+            ConnectionClosedByBroker,
+            StreamLostError,
+            ),
+        stop=stop_after_attempt(3),
+        wait=wait_fixed(2),
+        before_sleep=before_sleep_log(log, log.warning)
+           )
     def setup_connection(self, context: dict[str, str] | None = None):
         """We can use setup_blocking_connection for establishing a connection to RabbitMQ server specifying connection parameters.
         The connection is blocking which is only advisable to use for the apps with low througput. 
@@ -53,3 +69,110 @@ class MrsalBlockingAMQP(Mrsal):
             self.log.info(f"Connection established with RabbitMQ on {connection_info}")
         except (pika.exceptions.AMQPConnectionError, Exception) as err:
             self.log.error(f"I tried to connect with the RabbitMQ server but failed with: {err}")
+
+    def setup_exchange_and_queue(self, 
+                                 exchange_name: str, queue_name: str, exchange_type: str,
+                                 routing_key: str, exch_args: dict[str, str] | None = None,
+                                 queue_args: dict[str, str] | None = None,
+                                 bind_args: dict[str, str] | None = None,
+                                 exch_durable: bool = True, queue_durable: bool =True,
+                                 passive: bool = False, internal: bool = False,
+                                 auto_delete: bool = False, exclusive: bool = False
+                                 ):
+
+        self._declare_exchange(
+                exchange=exchange_name,
+                exchange_type=exchange_type,
+                arguments=exch_args,
+                durable=exch_durable,
+                passive=passive,
+                internal=internal,
+                auto_delete=auto_delete
+                )
+
+        self._declare_queue(
+                queue=queue_name,
+                arguments=queue_args,
+                durable=queue_durable,
+                passive=passive,
+                exclusive=exclusive,
+                auto_delete=auto_delete
+                )
+
+        self._declare_queue_binding(
+                exchange=exchange_name,
+                queue=queue_name,
+                routing_key=routing_key,
+                arguments=bind_args
+                )
+
+    def start_consumer(self,
+                       queue_name: str,
+                       callback: Callable,
+                       callback_args: dict[str, str | int | float | bool] | None = None,
+                       auto_ack: bool = True,
+                       inactivity_timeout: int = 5,
+                       auto_declare: bool = True,
+                       exchange_name: str | None = None,
+                       exhange_type: str | None = None,
+                       routing_key: str | None = None
+                       ):
+        """
+        Start the consumer using blocking setup.
+        :param queue: The queue to consume from.
+        :param auto_ack: If True, messages are automatically acknowledged.
+        :param inactivity_timeout: Timeout for inactivity in the consumer loop.
+        :param callback: The callback function to process messages.
+        :param callback_args: Optional arguments to pass to the callback.
+        """
+        if auto_declare:
+            if None in (exchange_name, queue_name, exhange_type, routing_key):
+                raise TypeError('Make sure that you are passing in all the necessary args for auto_declare')
+
+            self.setup_exchange_and_queue(
+                    exchange_name=exchange_name,
+                    queue_name=queue_name,
+                    exchange_type=exhange_type,
+                    routing_key=routing_key
+                    )
+        try:
+            for method_frame, properties, body in self._channel.consume(
+                    queue=queue_name, auto_ack=auto_ack, inactivity_timeout=inactivity_timeout
+                    ):
+                if method_frame:
+                    app_id = properties.app_id if properties else None
+                    msg_id = properties.msg_id if properties else None
+
+                    if self.verbose:
+                        self.log.info(
+                                """
+                                Message received with:
+                                - Method Frame: {method_frame)
+                                - Redelivery: {method_frame.redelivered}
+                                - Exchange: {method_frame.exchange}
+                                - Routing Key: {method_frame.routing_key}
+                                - Delivery Tag: {method_frame.delivery_tag}
+                                - Properties: {properties}
+                                """
+                                )
+                    if auto_ack:
+                        self.log.success(f'I successfully received a message from: {app_id} with messageID: {msg_id}')
+                    
+                    if callback:
+                        if callback_args:
+                            callback(*callback_args, method_frame, properties, body)
+                        else:
+
+                            callback( method_frame, properties, body)
+                else:
+                    # continue consuming
+                    continue
+        except Exception as e:
+            self.log.error(f'Oh lordy lord! I failed consuming ze messaj with: {e}')
+
+
+    @dataclass
+    class MrsalAsyncAMQP(Mrsal):
+        # NOTE! fill in for async
+        pass
+
--- mrsal/mrsal.py
+++ mrsal/mrsal.py
@@ -1,22 +1,16 @@
-import concurrent.futures
 import json
 import os
 import ssl
 import pika
 import asyncio
 from pydantic.dataclasses import dataclass
-from socket import gaierror
-from typing import Any, Callable, Dict, Tuple
+from typing import Any
 from pika import SSLOptions
 from pika.exceptions import ChannelClosedByBroker, ConnectionClosedByBroker
 from pika.exchange_type import ExchangeType
 from pika.adapters.asyncio_connection import AsyncioConnection
-from retry import retry
-
 from neolibrary.monitoring.logger import NeoLogger
 from config.exceptions import MissingTLSCerts
-
-from mrsal.utils import utils
 
 
 @dataclass
@@ -51,7 +45,7 @@ class Mrsal:
     prefetch_count: int = 1
     heartbeat: int = 600  # sec
     blocked_connection_timeout: int = 300  # sec
-    _connection: pika.BlockingConnection | None = None
+    _connection = None
     _channel = None
     log = NeoLogger(__name__, rotate_days=10)
 
@@ -128,12 +122,12 @@ class Mrsal:
         self.log.info("Async connection established.")
         asyncio.create_task(self.open_channel())
 
-    async def setup_exchange(self, 
-                       exchange: str, exchange_type: str,
-                       arguments: dict[str, str] | None = None,
-                       durable=True, passive=False,
-                       internal=False, auto_delete=False
-                       ):
+    async def _declare_exchange(self, 
+                             exchange: str, exchange_type: str,
+                             arguments: dict[str, str] | None,
+                             durable: bool, passive: bool,
+                             internal: bool, auto_delete: bool
+                            ):
         """This method creates an exchange if it does not already exist, and if the exchange exists, verifies that it is of the correct and expected class.
 
         If passive set, the server will reply with Declare-Ok if the exchange already exists with the same name,
@@ -181,10 +175,10 @@ class Mrsal:
         if self.verbose:
             self.log.info(f"Exchange is declared successfully with blocking set to {self.use_blocking}: {exchange_declare_info}")
 
-    def setup_queue(self,
-                    queue: str, arguments: dict[str, str] | None = None,
-                    durable: bool = True, exclusive: bool = False,
-                    auto_delete: bool = False, passive: bool = False
+    def _declare_queue(self,
+                    queue: str, arguments: dict[str, str] | None,
+                    durable: bool, exclusive: bool,
+                    auto_delete: bool, passive: bool
                     ):
         """Declare queue, create if needed. This method creates or checks a queue.
         When creating a new queue the client can specify various properties that control the durability of the queue and its contents,
@@ -222,10 +216,11 @@ class Mrsal:
         except Exception as e:
             self.log.error(f'Ooopsie boy: I failed declaring queue: {e}')
 
-    def setup_queue_binding(self, 
+    def _declare_queue_binding(self, 
                             exchange: str, queue: str,
-                            routing_key: str | None = None,
-                            arguments: dict[str , str] | None = None):
+                            routing_key: str | None,
+                            arguments: dict[str, str] | None
+                            ):
         """Bind queue to exchange.
 
         :param str queue: The queue to bind to the exchange
@@ -261,35 +256,7 @@ class Mrsal:
         context.load_cert_chain(certfile=self.tls_crt, keyfile=self.tls_key)
         return context
 
-    def stop_consuming(self, consumer_tag: str) -> None:
-        self._channel.stop_consuming(consumer_tag=consumer_tag)
-        self.log.info(f"Consumer is stopped, carry on. consumer_tag={consumer_tag}")
-
-    def close_channel(self) -> None:
-        self._channel.close()
-        self.log.info("Channel is closed, carry on")
-
-    def close_connection(self) -> None:
-        self.close_channel()
-        self._connection.close()
-        self.log.info("Connection is closed, carry on")
-
-    def queue_delete(self, queue: str):
-        self._channel.queue_delete(queue=queue)
-
-    def exchange_delete(self, exchange: str):
-        self._channel.exchange_delete(exchange=exchange)
-
-    def exchange_exist(self, exchange: str, exchange_type: ExchangeType):
-        exch_result: pika.frame.Method = self.setup_exchange(exchange=exchange, exchange_type=exchange_type, passive=True)
-        return exch_result
-
-    # NOTE! This is not a check but a setup function
-    def queue_exist(self, queue: str):
-        queue_result = self.setup_queue(queue=queue, passive=True)
-        # message_count1 = result1.method.message_count
-        return queue_result
-
+    # NOTE! polish this accordingly
     def publish_message(
         self,
         exchange: str,
@@ -297,7 +264,6 @@ class Mrsal:
         message: Any,
         exchange_type: ExchangeType = ExchangeType.direct,
         queue: str = None,
-        fast_setup: bool = False,
         prop: pika.BasicProperties = None,
     ):
         """Publish message to the exchange specifying routing key and properties.
@@ -313,111 +279,14 @@ class Mrsal:
         :raises UnroutableError: raised when a message published in publisher-acknowledgments mode (see `BlockingChannel.confirm_delivery`) is returned via `Basic.Return` followed by `Basic.Ack`.
         :raises NackError: raised when a message published in publisher-acknowledgements mode is Nack'ed by the broker. See `BlockingChannel.confirm_delivery`.
         """
-        if fast_setup:
-            # setting up the necessary connections
-            self.setup_exchange(exchange=exchange, exchange_type=exchange_type)
-            self.setup_queue(queue=queue)
-            self.setup_queue_binding(exchange=exchange, queue=queue, routing_key=routing_key)
-        else:
-            # Check if the necessary resources (exch & queue) are active
-            try:
-                self.exchange_exist(exchange=exchange, exchange_type=exchange_type)
-                if queue is not None:
-                    self.queue_exist(queue=queue)
-            except pika.exceptions.ChannelClosedByBroker as err:
-                self.log.error(f"Failed to check active resources. Cancel consumer. {str(err)}")
-                self._channel.cancel()
-                raise pika.exceptions.ChannelClosedByBroker(404, str(err))
-
+        # setting up the necessary connections
+        self._declare_exchange(exchange=exchange, exchange_type=exchange_type)
+        self._declare_queue(queue=queue)
+        self._declare_queue_binding(exchange=exchange, queue=queue, routing_key=routing_key)
         try:
             # Publish the message by serializing it in json dump
             self._channel.basic_publish(exchange=exchange, routing_key=routing_key, body=json.dumps(message), properties=prop)
             self.log.info(f"Message ({message}) is published to the exchange {exchange} with a routing key {routing_key}")
 
-            # The message will be returned if no one is listening
-            return True
         except pika.exceptions.UnroutableError as err1:
             self.log.error(f"Producer could not publish message:{message} to the exchange {exchange} with a routing key {routing_key}: {err1}", exc_info=True)
-            raise pika.exceptions.UnroutableError(404, str(err1))
-
-    @retry((gaierror, pika.exceptions.AMQPConnectionError, pika.exceptions.StreamLostError, pika.exceptions.ConnectionClosedByBroker, pika.exceptions.ChannelClosedByBroker), tries=15, delay=1, jitter=(2, 10), self.log=self.log)
-    def full_setup(
-            self,
-            exchange: str = None,
-            exchange_type: str = None,
-            arguments: Dict[str, str] = None,
-            routing_key: str = None,
-            queue: str = None,
-            callback: Callable = None,
-            requeue: bool = False,
-            callback_with_delivery_info: bool = False,
-            auto_ack: bool = False,
-    ) -> None:
-        """
-        Sets up the connection, exchange, queue, and consumer for interacting with a RabbitMQ server.
-
-        This method configures the connection to the RabbitMQ server and sets up the required messaging
-        components such as exchange, queue, and consumer. It also handles retries in case of connection failures.
-
-        Parameters
-        ----------
-        exchange : str, optional
-            The name of the exchange to declare. If `None`, no exchange will be declared. Default is `None`.
-        exchange_type : str, optional
-            The type of exchange to declare (e.g., 'direct', 'topic', 'fanout', 'headers'). Required if `exchange` is specified.
-            Default is `None`.
-        arguments : Dict[str, str], optional
-            A dictionary of additional arguments to pass when declaring the exchange or queue. Default is `None`.
-        routing_key : str, optional
-            The routing key to bind the queue to the exchange. This is used to determine which messages go to which queue.
-            Default is `None`.
-        queue : str, optional
-            The name of the queue to declare. If `None`, a randomly named queue will be created. Default is `None`.
-        callback : Callable, optional
-            A callback function to be executed when a message is received. The function should accept the message as a parameter.
-            Default is `None`.
-        requeue : bool, optional
-            If `True`, failed messages will be requeued. This is used in cases where you want to retry processing a message later.
-            Default is `False`.
-        callback_with_delivery_info : bool, optional
-            If `True`, the callback function will receive additional delivery information (e.g., delivery tag, redelivered flag).
-            Default is `False`.
-        auto_ack : bool, optional
-            If `True`, messages will be automatically acknowledged as soon as they are delivered to the consumer.
-            If `False`, messages need to be manually acknowledged. Default is `False`.
-
-        Returns
-        -------
-        None
-            This function does not return any value. It performs the setup and starts consuming messages.
-
-        Raises
-        ------
-        pika.exceptions.AMQPConnectionError
-            Raised if the connection to the RabbitMQ server fails after multiple retry attempts.
-        pika.exceptions.ChannelClosedByBroker
-            Raised if the channel is closed by the broker for some reason.
-        pika.exceptions.ConnectionClosedByBroker
-            Raised if the connection is closed by the broker.
-
-        Example
-        -------
-        >>> major_setup(
-                exchange='my_exchange',
-                exchange_type='direct',
-                routing_key='my_routing_key',
-                queue='my_queue',
-                callback=my_callback_function
-            )
-        """
-        self.connect_to_server()
-        self.setup_exchange(exchange=exchange, exchange_type=exchange_type, arguments=arguments)
-        self.setup_queue(queue=queue)
-        self.setup_queue_binding(exchange=exchange, queue=queue, routing_key=routing_key)
-        self.start_consumer(
-            queue=queue,
-            callback=callback,
-            requeue=requeue,
-            callback_with_delivery_info=callback_with_delivery_info,
-            auto_ack=auto_ack,
-        )
